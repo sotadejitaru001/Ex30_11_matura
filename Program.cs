6ba319c463080fa4dae7ee7d9c870701e722d56f
Jun Shimura <junshimura@gmail.com>
2023-12-19 16:34:50 +0900

    Update Program.cs
    さようならを追加

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Inheritance
         static void Main(string[] args)
         {
             Person person = new Person(DateTime.Now, "デジタル太郎", 0.5f, 160f, 50f);
-            Console.WriteLine("Hello World!こんにちは！");
+            Console.WriteLine("Hello World!こんにちは！さようなら");
         }
     }
 }
